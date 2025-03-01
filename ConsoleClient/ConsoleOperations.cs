f7bbe5d83e099d1bb82071706b71ce48bf1c3d96
aresceribilis <aresceribilis@gmail.com>
2025-03-01 23:01:08 +0200

    create ConsoleOperations enum

---
+++ ConsoleClient/ConsoleOperations.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleClient
+{
+    enum ConsoleOperations
+    {
+        AddTwoNumbers = 1,
+        SubstractTwoNumbers = 2,
+        MultiplyTwoNumbers = 3,
+        DivideTwoNumbers = 4,
+        PowerNumber = 5,
+        SqrtNumber = 6,
+        Abort = 7,
+    }
+}
